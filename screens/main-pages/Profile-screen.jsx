f468d110bfde48d6a955afe94917699e44298124
MrZeal025 <ryannograles.025.gtis@gmail.com>
2022-01-04 23:05:30 +0800

    added over ride update of profile information summary, added model pop up confirmation

--- screens/main-pages/Profile-screen.jsx
+++ screens/main-pages/Profile-screen.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 // native components
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Text, View, SafeAreaView, Modal, StyleSheet, Pressable } from 'react-native';
-import * as yup from 'yup';
+import { Text, View, SafeAreaView, Modal, StyleSheet, Pressable, TouchableOpacity } from 'react-native';
+// import * as yup from 'yup';
 import { Formik } from 'formik';
 import { AntDesign } from '@expo/vector-icons';
 import { CheckBox } from 'react-native-elements';
@@ -13,68 +13,68 @@ import KeyboardAvoidingWrapper from '../../_utils/KeyboardAvoidingWrapper';
 import CustomInputs from '../../_utils/CustomInputs';
 import CustomButton from '../../_utils/CustomButton';
 import { Colors } from '../../styles/styles-colors';
-import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
+import { ScrollView } from 'react-native-gesture-handler';
 import { FontAwesome } from '@expo/vector-icons';
+import { _setThisPageToCompleted } from '../../_storages/_state_process';
 
 // user profile validation schema
-let personalInfoSchema = yup.object().shape({
-  studentNumber: yup.string().nullable(),
-  facultyPosition: yup.string().nullable(),
-  jobTitle: yup.string().nullable(),
-  collegeDepartment: yup
-    .string()
-    .matches(/[A-Za-z]/, 'College department must contain only letters')
-    .nullable(),
-  firstName: yup
-    .string()
-    .matches(/[A-Za-z]/, 'First name must contain only letters')
-    .required('First name is required'),
-  middleName: yup
-    .string()
-    .matches(/[A-Za-z]/, 'Middle name must contain only letters')
-    .nullable(),
-  lastName: yup
-    .string()
-    .matches(/[A-Za-z]/, 'Last name must contain only letters')
-    .required('Last name is required'),
-  nameExtension: yup
-    .string()
-    .matches(/[A-Za-z.]/, 'Name extension name must contain only letters')
-    .nullable(),
-  lotNumber: yup
-    .string()
-    .matches(/[A-Za-z]/, 'Lot number must contain only letters')
-    .nullable(),
-  streetName: yup
-    .string()
-    .matches(/[A-Za-z]/, 'Street name must contain only letters')
-    .required('Street name is required'),
-  district: yup
-    .string()
-    .matches(/[A-Za-z]/, 'District must contain only letters')
-    .nullable(),
-  barangay: yup
-    .string()
-    .matches(/[A-Za-z.]/, 'Barangay name must contain only letters')
-    .required('Barangay is required'),
-  city: yup
-    .string()
-    .matches(/[A-Za-z.]/, 'City name must contain only letters')
-    .required('City is required'),
-  province: yup
-    .string()
-    .matches(/[0-9A-Za-z.]/, 'Province name must contain only letters')
-    .required('Province is required'),
-});
+// let personalInfoSchema = yup.object().shape({
+//   studentNumber: yup.string().nullable(),
+//   facultyPosition: yup.string().nullable(),
+//   jobTitle: yup.string().nullable(),
+//   collegeDepartment: yup
+//     .string()
+//     .matches(/[A-Za-z]/, 'College department must contain only letters')
+//     .nullable(),
+//   firstName: yup
+//     .string()
+//     .matches(/[A-Za-z]/, 'First name must contain only letters')
+//     .required('First name is required'),
+//   middleName: yup
+//     .string()
+//     .matches(/[A-Za-z]/, 'Middle name must contain only letters')
+//     .nullable(),
+//   lastName: yup
+//     .string()
+//     .matches(/[A-Za-z]/, 'Last name must contain only letters')
+//     .required('Last name is required'),
+//   nameExtension: yup
+//     .string()
+//     .matches(/[A-Za-z.]/, 'Name extension name must contain only letters')
+//     .nullable(),
+//   lotNumber: yup
+//     .string()
+//     .matches(/[A-Za-z]/, 'Lot number must contain only letters')
+//     .nullable(),
+//   streetName: yup
+//     .string()
+//     .matches(/[A-Za-z]/, 'Street name must contain only letters')
+//     .required('Street name is required'),
+//   district: yup
+//     .string()
+//     .matches(/[A-Za-z]/, 'District must contain only letters')
+//     .nullable(),
+//   barangay: yup
+//     .string()
+//     .matches(/[A-Za-z.]/, 'Barangay name must contain only letters')
+//     .required('Barangay is required'),
+//   city: yup
+//     .string()
+//     .matches(/[A-Za-z.]/, 'City name must contain only letters')
+//     .required('City is required'),
+//   province: yup
+//     .string()
+//     .matches(/[0-9A-Za-z.]/, 'Province name must contain only letters')
+//     .required('Province is required'),
+// });
 
 const ProfileScreen = () => {
   // default states
   const [modalVisible, setModalVisible] = useState(false);
-  const [userType, setUserType] = useState('');
+  const [modalConfirmVisible, setModalConfirmVisible] = useState(false);
   const [prevInfo, setPrevInfo] = useState({});
 
   const setUserTypeChoice = type => {
-    setUserType(type);
     setPrevInfo({ ...prevInfo, userType: type });
   };
 
@@ -92,6 +92,16 @@ const ProfileScreen = () => {
     } catch (error) {
       console.log(error);
     }
+  };
+
+  // this will check if the app data are all loaded
+  const checkIfAppReady = () => {
+    AsyncStorage.getItem('@profileInfo')
+      .then(res => {
+        const newdata = JSON.parse(res);
+        console.log(newdata);
+      })
+      .catch(error => console.warn(error));
   };
 
   // this function is a react native lifecycle method that will run when a component is mounted / loaded
@@ -180,31 +190,14 @@ const ProfileScreen = () => {
         <SafeAreaView style={landingPagesOrientation.container}>
           <Text style={displayFormContainer.formsHeader}>Profile Information Summary</Text>
           <Formik
-            initialValues={{
-              userType: '',
-              studentNumber: '',
-              facultyPosition: '',
-              jobTitle: '',
-              collegeDepartment: '',
-              firstName: '',
-              middleName: '',
-              lastName: '',
-              nameExtension: '',
-              lotNumber: '',
-              streetName: '',
-              district: '',
-              barangay: '',
-              city: '',
-              province: '',
-              mobileNumber: '',
-            }}
+            initialValues={prevInfo}
             validateOnMount={true}
-            validationSchema={personalInfoSchema}
-            onSubmit={values => {
-              console.log(values);
+            //validationSchema={personalInfoSchema}
+            onSubmit={() => {
+              setModalConfirmVisible(true);
             }}
           >
-            {({ handleBlur, handleSubmit, errors, touched }) => (
+            {({ handleBlur, handleSubmit, errors, touched, values }) => (
               <View style={{ marginTop: 10 }}>
                 <View style={{ marginBottom: 15 }}>
                   <Text style={displayFormContainer.formCaptions}>User Affiliation</Text>
@@ -355,6 +348,7 @@ const ProfileScreen = () => {
               </View>
             )}
           </Formik>
+          {/* option modal for changing user affiliation */}
           <Modal
             animationType="slide"
             transparent={true}
@@ -366,7 +360,7 @@ const ProfileScreen = () => {
             <View style={styles.centeredView}>
               <View style={styles.modalView}>
                 <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-                  <Text style={styles.modalText}>Change user position?</Text>
+                  <Text style={styles.modalText}>Change user affiliation?</Text>
                   <Pressable onPress={() => setModalVisible(!modalVisible)}>
                     <Text style={{ color: 'black' }}>
                       <FontAwesome name="times" color="black" size={22} />
@@ -374,7 +368,7 @@ const ProfileScreen = () => {
                   </Pressable>
                 </View>
                 <CheckBox
-                  checked={userType === 'Student'}
+                  checked={prevInfo.userType === 'Student'}
                   checkedIcon="dot-circle-o"
                   uncheckedIcon="circle-o"
                   checkedColor={Colors.primary}
@@ -383,7 +377,7 @@ const ProfileScreen = () => {
                   title="Student"
                 />
                 <CheckBox
-                  checked={userType === 'Faculty'}
+                  checked={prevInfo.userType === 'Faculty'}
                   checkedIcon="dot-circle-o"
                   uncheckedIcon="circle-o"
                   checkedColor={Colors.primary}
@@ -392,7 +386,7 @@ const ProfileScreen = () => {
                   title="Faculty"
                 />
                 <CheckBox
-                  checked={userType === 'Worker'}
+                  checked={prevInfo.userType === 'Worker'}
                   checkedIcon="dot-circle-o"
                   uncheckedIcon="circle-o"
                   checkedColor={Colors.primary}
@@ -401,7 +395,7 @@ const ProfileScreen = () => {
                   title="Worker"
                 />
                 <CheckBox
-                  checked={userType === 'Visitor / Guest'}
+                  checked={prevInfo.userType === 'Visitor / Guest'}
                   checkedIcon="dot-circle-o"
                   uncheckedIcon="circle-o"
                   checkedColor={Colors.primary}
@@ -409,6 +403,71 @@ const ProfileScreen = () => {
                   containerStyle={checkBox.radioOptions}
                   title="Visitor / Guest"
                 />
+              </View>
+            </View>
+          </Modal>
+          {/* confirm modal for saving the data */}
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalConfirmVisible}
+            onRequestClose={() => {
+              setModalConfirmVisible(!modalConfirmVisible);
+            }}
+          >
+            <View style={styles.centeredView}>
+              <View style={[styles.modalView]}>
+                <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+                  <Text style={styles.modalText}>Do you want to save the changes?</Text>
+                </View>
+                <View
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    width: '100%',
+                    marginTop: 15,
+                  }}
+                >
+                  <TouchableOpacity
+                    style={{ width: '50%' }}
+                    onPress={() => setModalConfirmVisible(!modalConfirmVisible)}
+                  >
+                    <View
+                      style={{
+                        backgroundColor: Colors.lightGrey,
+                        height: 50,
+                        justifyContent: 'center',
+                        marginRight: 10,
+                        alignItems: 'center',
+                        borderRadius: 3,
+                      }}
+                    >
+                      <Text style={{ fontSize: 16, fontWeight: '700' }}>No</Text>
+                    </View>
+                  </TouchableOpacity>
+                  {/* yes button */}
+                  <TouchableOpacity
+                    style={{ width: '50%' }}
+                    onPress={() => {
+                      _setThisPageToCompleted('@profileInfo', JSON.stringify(prevInfo));
+                      setModalConfirmVisible(!modalConfirmVisible);
+                    }}
+                  >
+                    <View
+                      style={{
+                        backgroundColor: Colors.accent,
+                        height: 50,
+                        marginLeft: 10,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        borderRadius: 3,
+                      }}
+                    >
+                      <Text style={{ fontSize: 16, fontWeight: '700', color: 'white' }}>Yes</Text>
+                    </View>
+                  </TouchableOpacity>
+                </View>
               </View>
             </View>
           </Modal>
